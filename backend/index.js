a720c9707953d23d8e8ad7445b9c1abe1b73dc87
Meghraj-parashar <meghrajparashar@gmail.com>
2025-03-27 13:17:15 +0530

    sending json data from server to client(react)

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = 3000;
+// app.use(cors("origin","http://localhost:5173/"));
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({"Hello": "World'"});
+  
+});
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
