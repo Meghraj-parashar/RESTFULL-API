ac829d7a7a974a02407fdbe05c9a2f679e1f10d1
Meghraj-parashar <meghrajparashar@gmail.com>
2025-03-27 14:53:12 +0530

    using axios to connect client to server(react)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-
+import axios from 'axios'
 function App() {
   async function getRes() {
-    try {
-      const res = await fetch('http://localhost:3000')
-      const data = await res.json()
-      console.log(data) // data is already parsed JSON
-    } catch (error) {
+                 //connecting using fetch
+    // try {
+    //   const res = await fetch('http://localhost:3000')
+    //   const data = await res.json()
+    //   console.log(data) // data is already parsed JSON
+    // } catch (error) {
+    //   console.error('Error fetching data:', error)
+    // }
+                //connecting using axios
+    axios.get('http://localhost:3000')
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((error) => {
       console.error('Error fetching data:', error)
-    }
+    })  
+    
+          
   }
 
   return (
